ceae0b9cc49167ad24c01789e15a25a247702616
Milan <milandahal213@gmail.com>
2023-05-07 12:26:02 -0400

    Update pfmqtt.py

--- Final/pfmqtt.py
+++ Final/pfmqtt.py
@@ -6,14 +6,24 @@
 #
 import paho.mqtt.client as mqtt #import the client1
 import time
+import json
 
+val=''
 def whenCalled(client, userdata, message):
-    print("received " ,str(message.payload.decode("utf-8")))
-    print("from topic ",message.topic)
+    global val
+    val+=str(message.payload.decode("utf-8"))
 
-broker = '192.168.86.31'
-topic_sub = "ESP/tell"
-topic_pub = "ESP/listen"
+
+def checkvals():
+    global val
+    print(val)
+    value=json.loads(val)
+    val=''
+    return value
+
+broker = '10.245.144.196'
+topic_sub = "milan/tell"
+topic_pub = "milan/listen"
 
 client = mqtt.Client("fred") # use a unique name
 client.on_message = whenCalled # callback
@@ -25,8 +35,22 @@ client.subscribe(topic_sub)
 
 i = 0
 while True:
-    print("Publishing message to topic",topic_pub)
-    client.publish(topic_pub,"iteration %d" % i)
-    time.sleep(10) # wait for a little
-		i+=1
+    getch= input()
+    print("moving the robot")
+    client.publish(topic_pub,'1')
+    #getch= input()
+
+    while(val.find('end')<0):
+        time.sleep(0.1)
+        print(val)
+        pass
+    print(val)
+    datafromrobot=checkvals()
+    print(datafromrobot)
+    print("E",datafromrobot["E"])
+    print("W",datafromrobot["W"])
+    print("N",datafromrobot["N"])
+    print("S",datafromrobot["S"])
+
+
 client.loop_stop() #stop the loop
